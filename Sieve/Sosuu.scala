0c6117d8ef97d4c4f6e9af881c53e4841ae85f7e
rumagishi <rumagishi@gmail.com>
2015-03-27 16:54:53 +0900

    modified Sosuu.scala

--- Sieve/Sosuu.scala
+++ Sieve/Sosuu.scala
@@ -4,21 +4,14 @@ object Sosuu {
 
   def furui(list: (List[Int], List[Int]), listlast: Int): List[Int] = {
     val head = list._1.head
-    head match {
-      case 1 => furui((list._1.tail, List()), listlast)
-      case _ =>
-        if(pow(listlast,0.5)>head) 
-          furui((list._1.filter(_%head!=0), (head :: list._2.reverse).reverse), listlast) 
-        else
-          list._2 ++ list._1
-    }
+    if(pow(listlast,0.5)>head) 
+      furui((list._1.filter(_%head!=0), (head :: list._2.reverse).reverse), listlast) 
+    else
+      list._2 ++ list._1
   }
 
   def main(args: Array[String]) {
-    val list = (1 to 30).toList
+    val list = (2 to 30).toList
     println(furui((list, List()), list.last))
   }
-
 }
-
-
